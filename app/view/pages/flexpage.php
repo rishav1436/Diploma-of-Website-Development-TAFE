20891bfb14f2f79d566ffd567b0d891eb39ac2f7
Harris Fauntleroy <7843312+HarrisFauntleroy@users.noreply.github.com>
2020-08-26 11:02:19 +1000

    changes to workflow

---
+++ app/view/pages/flexpage.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ML Strength Brisbane">
+    <title>ML Strength Brisbane</title>
+    <link rel="stylesheet" type="text/css" href="/public/css/flex-page-stylesheet.css?v=3">
+</head>
+
+<body>
+
+    <header>
+        Background image
+        <a>1</a>
+        <a>2</a>
+    </header>
+
+    <nav>
+
+        <a id="navhome" href="/index.php">1</a>
+
+        <a id="navcontact" href="/app/view/pages/content.php">2</a>
+
+        <a id="navlogin" href="/app/view/pages/table-of-data.php">3</a>
+
+        <a href="/app/view/pages/web-form.php">4</a>
+
+        <a href="/app/view/pages/blog.php">5</a>
+
+    </nav>
+
+    <main>
+
+        <aside>
+            <a>1</a>
+            <a>2</a>
+            <a>3</a>
+            <a>4</a>
+            <a>5</a>
+        </aside>
+
+        <section>
+            Content area
+        </section>
+
+    </main>
+
+    <footer>
+        <a>1</a>
+        <a>2</a>
+        <a>3</a>
+        <a>4</a>
+        <a>5</a>
+    </footer>
+
+</body>
+
+</html>
